df717bdb9fe066395c3e96a5e852211038fcbd2b
Andy Chen <amazingandyyy@gmail.com>
2018-03-03 14:44:15 -0800

    add no log in webpack prod

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const NoConsolePlugin = require('no-console-webpack-plugin');
 
 const common = require('./webpack.common.js');
 
@@ -10,6 +11,13 @@ module.exports = merge(common, {
                 'NODE_ENV': JSON.stringify('production'),
                 'SERVERURI': JSON.stringify('https://server.revieweer.com')
             }
+        }),
+        new NoConsolePlugin({
+            ignores:[
+                'log',
+                'info',
+                'warn'
+            ]
         })
     ]
 })
