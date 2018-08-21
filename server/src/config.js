ca770d7ab50740a1c1bb972166502ae9b8c479ff
Andy Chen <amazingandyyy@gmail.com>
2018-08-20 22:53:31 -0700

    update versions

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 import path from 'path';
-import path from 'path';
 if(process.env.NODE_ENV != 'production'){
   dotenv.config({ path: path.resolve(__dirname, '.env') });
 }
